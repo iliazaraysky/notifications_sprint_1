8a8d040686dd3fe24a1c5c4b5c7547b5de43a94e
Ilia Zaraysky <nextontext@yandex.ru>
2022-07-05 17:19:12 +0300

    Удалил закомментированные части кода

--- api/main.py
+++ api/main.py
@@ -1,9 +1,8 @@
-import aiohttp
 import uvicorn
 from db import mongodb
 from motor.motor_asyncio import AsyncIOMotorClient
 from api.v1 import rabbitmq_api, user_event_api
-from fastapi import FastAPI, Response, Request
+from fastapi import FastAPI
 from fastapi.responses import ORJSONResponse
 
 
@@ -28,25 +27,6 @@ async def shutdown():
 app.include_router(rabbitmq_api.router, prefix='/api/v1/rabbitmq', tags=['rabbitmq_event'])
 app.include_router(user_event_api.router, prefix='/api/v1/films', tags=['user_event'])
 
-
-# Проверяет Auth сервис. Обращается по адресу.
-# Если в заголовке есть валидный токен, предоставляет доступ к контенту
-# @app.middleware('http')
-# async def add_process_time_header(request: Request, call_next):
-#     headers = request.headers
-#     auth_url = 'http://notifications-flask-auth-service:5000/auth/v1/usercheck'
-#     auth_check = await check_user(auth_url, dict(headers))
-#
-#     if auth_check.status == 200:
-#         response = await call_next(request)
-#         return response
-#     return Response(status_code=401)
-#
-#
-# async def check_user(url, headers):
-#     async with aiohttp.ClientSession(headers=headers) as session:
-#         async with session.get(url) as response:
-#             return response
 
 if __name__ == '__main__':
     uvicorn.run(
